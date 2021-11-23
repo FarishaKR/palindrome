66314aa74ab378844873752c8a2cd4fa244127a4
Farisha KR <73113669+FarishaKR@users.noreply.github.com>
2021-11-23 23:29:08 +0530

    sum

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int a, b;
+    int sum = 0;
+    
+    printf("Enter the first number:");
+    scanf("%s", a);
+     printf("Enter the first number:");
+    scanf("%s", b);
+    sum=a+b;
+   printf("sum of given number",sum);
+   
+}
