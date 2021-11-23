f8b6d602fc221d1e28039084307b6a405a6fcd31
Farisha KR <73113669+FarishaKR@users.noreply.github.com>
2021-11-23 23:26:06 +0530

    new

---
+++ palindrome.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char s[20];
+    int i, length;
+    int flag = 0;
+    
+    printf("Enter a string:");
+    scanf("%s", s);
+    
+    length = strlen(s);
+    
+    for(i=0;i < length ;i++){
+        if(s[i] != s[length-i-1]){
+            flag = 1;
+            break;
+   }
+}
+    
+    if (flag) {
+        printf("%s is not a palindrome", s);
+    }    
+    else {
+        printf("%s is a palindrome", s);
+    }
+    return 0;
+}
